cda935e160aaf8083f2fa412fabc422b4e204229
omar <omar_209omardf@hotmail.com>
2022-07-21 09:06:27 +0200

    init

---
+++ lib/modules/users/users_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class UsersScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('We Will Add Map Soon'));
+  }
+}
